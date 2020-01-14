0c058de90ab573a1f5e0e730b71938e803f72749
hansaemgo <rhgkstoa7@gmail.com>
2020-01-14 17:58:03 +0900

    outline, fullwidth props설정

--- src/App.js
+++ src/App.js
@@ -10,7 +10,21 @@ function App() {
         <Button size="large">BUTTION</Button>
         <Button size="medium">BUTTION</Button>
         <Button size="small">BUTTION</Button>
-
+      </div>
+      <div className="buttons">
+        <Button color="gray" size="large">BUTTION</Button>
+        <Button color="gray" size="medium">BUTTION</Button>
+        <Button color="gray" size="small">BUTTION</Button>
+      </div>
+      <div className="buttons">
+        <Button color="pink">BUTTION</Button>
+        <Button color="pink" size="medium">BUTTION</Button>
+        <Button color="pink" size="small">BUTTION</Button>
+      </div>
+      <div className="buttons">
+        <Button outline={true}>BUTTON</Button>
+        <Button color="pink" outline >BUTTION</Button>
+        <Button color="gray" outline>BUTTION</Button>
       </div>
     </div>
   );
--- src/components/Button.js
+++ src/components/Button.js
@@ -5,12 +5,21 @@ import './Button.scss';
 
 // 다양한 버튼 크기 만들기
 // large, medium, small
-function Button({children, size }) {
-    return <button className ={classNames('Button', size)}>{children}</button>
+// 다양한 버튼 컬러 생성
+// color: blue, pink, gray
+
+function Button({children, size, color, outline, fullWidth }) {
+    return (
+    <button className ={classNames('Button', size, color, {
+        outline,
+        fullWidth
+    })}>{children}</button>
+    )
 };
 
 Button.defaultProps = {
-    size : 'medium'
+    size : 'medium',
+    color: 'blue'
 };
 
 

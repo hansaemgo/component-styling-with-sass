981869e9a61b6153bd25c261e016f3f1100d433a
hansaemgo <rhgkstoa7@gmail.com>
2020-02-03 15:16:11 +0900

    CSS Module ( classnames, react-icons )

--- src/App.js
+++ src/App.js
@@ -1,57 +1,16 @@
-import React from 'react';
-import './App.scss';
-import Button from './components/Button';
+import React, { useState } from 'react';
+import CheckBox from './components/CheckBox';
 
 function App() {
+	const [check, setCheck] = useState(false);
+	const onChange = (e) => {
+		setCheck(e.target.checked);
+	};
 	return (
-		<div className="App">
-			<div className="Buttons">
-				<Button size="large">Button</Button>
-				<Button>Button</Button>
-				<Button size="small">Button</Button>
-			</div>
-			<div className="Buttons">
-				<Button color="gray" size="large">
-					Button
-				</Button>
-				<Button color="gray">Button</Button>
-				<Button color="gray" size="small">
-					Button
-				</Button>
-			</div>
-			<div className="Buttons">
-				<Button color="orange" size="large">
-					Button
-				</Button>
-				<Button color="orange">Button</Button>
-				<Button color="orange" size="small">
-					Button
-				</Button>
-			</div>
-			<div className="Buttons">
-				<Button color="orange" outline size="large">
-					Button
-				</Button>
-				<Button color="orange" outline>
-					Button
-				</Button>
-				<Button color="orange" size="small" outline>
-					Button
-				</Button>
-			</div>
-			<div className="Buttons">
-				<Button fullwidth>Button</Button>
-				<Button color="orange" fullwidth>
-					Button
-				</Button>
-				<Button
-					color="gray"
-					fullwidth
-					onClick={() => console.log('클릭')}
-				>
-					Button
-				</Button>
-			</div>
+		<div>
+			<CheckBox onChange={onChange} checked={check}>
+				다음 약관에 모두 동의
+			</CheckBox>
 		</div>
 	);
 }
